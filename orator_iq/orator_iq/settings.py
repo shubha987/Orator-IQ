5edc36f3e87861d0568538356db694399582b90b
SwarDesai <139445228+SwarDesai@users.noreply.github.com>
2024-12-12 01:07:08 +0530

    Update settings.py

--- orator_iq/orator_iq/settings.py
+++ orator_iq/orator_iq/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+# Add this to the settings.py
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
